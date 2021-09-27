569e4d26f6f849ea2c9a5572d469f37ed8f7d513
audubilly <audubilly89@gmail.com>
2021-09-27 23:13:43 +0100

    created the publisher Api

--- src/main/java/com/billy/userservice/config/MQConfig.java
+++ src/main/java/com/billy/userservice/config/MQConfig.java
@@ -1,10 +1,11 @@
 package com.billy.userservice.config;
 
 import com.rabbitmq.client.AMQP;
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -25,12 +26,26 @@ public class MQConfig {
         return new TopicExchange(EXCHANGE);
     }
 
+    @Bean
     public Binding binding(Queue queue, TopicExchange exchange){
         return BindingBuilder
                 .bind(queue)
                 .to(exchange)
                 .with(ROUTING_KEY);
     }
+
+    @Bean
+    public MessageConverter  messageConverter(){
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public AmqpTemplate template(ConnectionFactory connectionFactory){
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(messageConverter());
+        return template;
+    }
+
 
 
 }
